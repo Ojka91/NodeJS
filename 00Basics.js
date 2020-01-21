353c918af696d36fe807272570da3be3f6518cef
Ojka91 <ojkalp@gmail.com>
2020-01-21 17:15:27 +0100

    First commit

---
+++ 00Basics.js
@@ -0,0 +1,13 @@
+/* Basics */
+
+//Enable strict mode
+'use strict'
+
+console.log('Hey dude, this is node');
+
+var x = 5;
+var y = 2;
+
+console.log(x+y);
+
+setInterval(() => console.log('Hey'), 1000);
---
+++ 02HttpServer.js
@@ -0,0 +1,20 @@
+/* Basic Script to set up an http server 
+    https://nodejs.org/en/about/ 
+*/
+
+'use strict'
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
