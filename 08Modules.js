423930a267a49e8a0c3c2ab8717fb0b2b59c7cc3
Ojka91 <ojkalp@gmail.com>
2020-02-05 17:08:05 +0100

    08 added

---
+++ 08Modules.js
@@ -0,0 +1,20 @@
+/**
+ * Exporting data from other places with require (accepts .js and .json)
+ */
+
+
+'use strict'
+
+var myData = require('./my-data'), 
+    Clock = require('./clock-es5'),
+    cucu = new Clock()
+    
+console.log(
+    myData.name + ' ' + myData.email
+)
+
+
+cucu.on('tictac', function () {
+    cucu.theTime()
+})
+
